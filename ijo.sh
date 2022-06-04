23ba49b11e33ebc507ec2f9dc99ffbcc393cdabc
koasdu <106860379+koasdu@users.noreply.github.com>
2022-06-04 07:38:06 +0000

    Update ijo.sh

--- ijo.sh
+++ ijo.sh
@@ -1,3 +1,9 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_ubuntu_18.tar.gz
-tar xf hellminer_cpu_ubuntu_18.tar.gz
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.GASSPOLL -p x --cpu 39
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y &&
+sudo apt install git -y &&
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git 
+cd ccminer &&
+chmod +x build.sh &&
+chmod +x configure.sh &&
+chmod +x autogen.sh &&
+./build.sh &&
+./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.Circl -p x -x socks5://192.252.211.197:14921
